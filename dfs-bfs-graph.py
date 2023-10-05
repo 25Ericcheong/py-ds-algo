c30f19bba57b8152c8739606e5c55280e5cad274
25Ericcheong <ericcheongkaikit@gmail.com>
2023-10-05 23:07:45 +1000

    cool implementation of dfs and bfs

---
+++ dfs-bfs-graph.py
@@ -0,0 +1,41 @@
+graph_data = {
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': ['F'],
+  'F': []
+}
+
+visited_store = []  # List to keep track of visited nodes.
+queue = []  # Initialize a queue
+
+
+# traversal printing of tree
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        s = queue.pop(0)
+        print(s, end=" ")
+
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+visited_set = set()
+
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+
+print(bfs(visited_store, graph_data, 'A'))
+print(dfs(visited_set, graph_data, 'A'))
