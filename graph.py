3631476f5a4d457a103fe1345768493d9158e808
25Ericcheong <ericcheongkaikit@gmail.com>
2023-10-05 22:18:30 +1000

    interesting find all possible pathways in graph

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
-graph = {'A': ['B', 'C'],
-         'B': ['C', 'D'],
-         'C': ['D'],
-         'D': ['C'],
-         'E': ['F'],
-         'F': ['C']}
+graph_data = {'A': ['B', 'C'],
+              'B': ['C', 'D'],
+              'C': ['D'],
+              'D': ['C'],
+              'E': ['F'],
+              'F': ['C']}
+
 
 # backtracking algorithm - tries all paths
 def find_path(graph, start, end, path=[]):
@@ -23,4 +24,24 @@ def find_path(graph, start, end, path=[]):
     return None
 
 
-print(find_path(graph, 'A', 'D'))
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+
+    if start not in graph:
+        return []
+
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            new_paths = find_all_paths(graph, node, end, path)
+            print(new_paths)
+
+            for new_path in new_paths:
+                paths.append(new_path)
+    return paths
+
+
+print(find_path(graph_data, 'A', 'D'))
+print(find_all_paths(graph_data, 'A', 'D'))
