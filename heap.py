0764ef20c2892be6980342cd6f6f6834a9435423
25Ericcheong <ericcheongkaikit@gmail.com>
2023-10-06 17:11:41 +1000

    more methods to be used with heap data structure

--- heap.py
+++ heap.py
@@ -1,8 +1,30 @@
 import heapq
 
-li = [5, 2, 4, 3, 1]
+li = [5, 7, 9, 1, 3]
 
 heapq.heapify(li)
 
-print("created heap is : ", (list(li)))
+# take not of pattern
+print("created heap is :", (list(li)))
 
+heapq.heappush(li, 4)
+print("modified heap after push is :", list(li))
+
+print("")
+
+li1 = [5, 1, 9, 4, 3]
+li2 = [5, 7, 9, 4, 3]
+
+heapq.heapify(li1)
+heapq.heapify(li2)
+
+# pops 2
+print("popped item using heappushpop is : ", end="")
+print(heapq.heappushpop(li1, 2))
+print("modified heap after heappushpop is :", list(li1))
+
+
+# pops 3
+print("popped item using heapreplace is : ", end="")
+print(heapq.heapreplace(li2, 2))
+print("modified heap after heapreplace is :", list(li2))
